716bc07e9904e6f928c592c37091039702b481ad
nathan.yang <nathan.yang@huolala.cn>
2021-08-21 11:20:19 +0800

    Feature - add WebClient test

--- ex_codec_portal/src/test/java/com/nathan/ex/WebFluxApplicationTests.java
+++ ex_codec_portal/src/test/java/com/nathan/ex/WebFluxApplicationTests.java
@@ -5,6 +5,8 @@ import com.nathan.ex.function.RunFunction;
 import com.nathan.ex.function.RunHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class WebFluxApplicationTests {
@@ -20,6 +22,20 @@ class WebFluxApplicationTests {
 
         RunFunction<String> runFunction = RunHandler::fly;
         FunctionCaller.call(runFunction);
+
+        WebClient webClient = WebClient.create();
+        Mono<String> mono = webClient.post()
+                .uri("http://localhost:8080/bye")
+                .bodyValue("{\"borrowerId\": \"12346309\"}")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        mono.subscribe(System.out::println);
+
+        System.out.println("webclient");
+
+        mono.block();
+
     }
 
 }
